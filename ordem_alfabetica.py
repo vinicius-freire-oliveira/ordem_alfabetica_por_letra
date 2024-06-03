1afce712c0ee651063d91bc329c328525753cf39
Vinícius-Freire <120794204+vinicius-freire-oliveira@users.noreply.github.com>
2024-06-03 10:12:53 -0300

    Add files via upload
    Ordena as letras da palavra em ordem alfabética

---
+++ ordem_alfabetica.py
@@ -0,0 +1,7 @@
+palavra = input("Digite a palavra: ")
+
+# Ordena as letras da palavra em ordem alfabética
+palavra_ordenada = "".join(sorted(palavra))
+
+# Imprime a palavra ordenada
+print("A odem alfabética por letra é:", palavra_ordenada)
